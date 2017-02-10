857042e94da243c9a7a0e4868a0acd1e0ece529d
Rapolas <rapolas.saulius@gmail.com>
2017-02-10 16:24:58 +0200

    first commit

---
+++ src/lt/swedbank/itacademy/git/Main.java
@@ -0,0 +1,10 @@
+package lt.swedbank.itacademy.git;
+
+/**
+ * Created by Rapolo on 2017-02-10.
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("VEIKIA");
+  }
+}
